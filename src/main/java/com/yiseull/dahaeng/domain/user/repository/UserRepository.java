f363e96dff29fd029c3c7249a0df7f4c73f81f24
Yiseull <omjl5123@catholic.ac.kr>
2023-04-02 19:27:31 +0900

    Refactor: 오버라이드 메소드 제거

--- src/main/java/com/yiseull/dahaeng/domain/user/repository/UserRepository.java
+++ src/main/java/com/yiseull/dahaeng/domain/user/repository/UserRepository.java
@@ -9,13 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Override
-    <S extends User> S save(S entity);
-
     Optional<User> findByEmailAndPassword(String email, String password);
-
-    @Override
-    void deleteById(Integer integer);
 
     boolean existsByEmail(String email);
 
