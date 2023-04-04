e1031dbf9d4d87cf9cd84b284284e4f89446f90d
Yiseull <omjl5123@catholic.ac.kr>
2023-04-05 01:18:40 +0900

    Fix: ErrorCode 수정

--- src/main/java/com/yiseull/dahaeng/exception/ErrorCode.java
+++ src/main/java/com/yiseull/dahaeng/exception/ErrorCode.java
@@ -11,8 +11,8 @@ public enum ErrorCode {
     // 회원
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "1000", "회원을 찾을 수 없습니다."),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "1001", "중복된 닉네임입니다."),
-    BAD_PASSWORD(HttpStatus.BAD_REQUEST, "1002", "잘못된 비밀번호입니다."),
-    BAD_EMAIL(HttpStatus.BAD_REQUEST, "1003", "잘못된 이메일입니다.");
+    BAD_PASSWORD(HttpStatus.BAD_REQUEST, "1002", "비밀번호가 일치하지 않습니다."),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "1003", "등록된 이메일을 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
