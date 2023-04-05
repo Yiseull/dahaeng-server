4fd27292ecee372516fe1e6ab9a5f850387769ae
Yiseull <omjl5123@catholic.ac.kr>
2023-04-06 00:23:27 +0900

    Add: Swagger 에 대한 권한 허용

--- src/main/java/com/yiseull/dahaeng/config/SecurityConfig.java
+++ src/main/java/com/yiseull/dahaeng/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
                 .cors().disable()
                 .csrf().disable()
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers("/api/users/**", "/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
