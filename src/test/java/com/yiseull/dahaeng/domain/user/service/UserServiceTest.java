91c93bfaefb5805c002926094185a0aa14af83fe
Yiseull <omjl5123@catholic.ac.kr>
2023-04-19 17:38:25 +0900

    Test: 로그인 성공 테스트 작성

--- src/test/java/com/yiseull/dahaeng/domain/user/service/UserServiceTest.java
+++ src/test/java/com/yiseull/dahaeng/domain/user/service/UserServiceTest.java
@@ -2,7 +2,9 @@ package com.yiseull.dahaeng.domain.user.service;
 
 import com.yiseull.dahaeng.domain.user.User;
 import com.yiseull.dahaeng.domain.user.dto.UserRequest;
+import com.yiseull.dahaeng.domain.user.dto.UserResponse;
 import com.yiseull.dahaeng.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -89,6 +93,51 @@ class UserServiceTest {
 
             // then
             assertThatThrownBy(() -> userService.signUp(request)).hasMessage("중복된 이메일입니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("로그인 테스트")
+    class login {
+
+        private User user;
+
+        @BeforeEach
+        void setUp() {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            user = User.builder()
+                    .userId(1)
+                    .email("test123@naver.com")
+                    .password(encoder.encode("test123"))
+                    .nickname("테스트다옹")
+                    .userColor(3)
+                    .build();
+        }
+
+        @Test
+        @DisplayName("로그인 성공")
+        void login_success() {
+
+            // given
+            UserRequest.Login request = UserRequest.Login.builder()
+                    .email("test123@naver.com")
+                    .password("test123")
+                    .build();
+
+            given(userRepository.findByEmail(anyString())).willReturn(Optional.ofNullable(user));
+
+            // when
+            UserResponse.Profile loginUser = userService.login(request);
+
+            // then
+            assertThat(user.getUserId()).isEqualTo(loginUser.getUserId());
+            assertThat(user.getEmail()).isEqualTo(loginUser.getEmail());
+            assertThat(user.getNickname()).isEqualTo(loginUser.getNickname());
+            assertThat(user.getUserColor()).isEqualTo(loginUser.getUserColor());
+
+            // verify
+            verify(userRepository, times(1)).findByEmail(anyString());
+            verify(passwordEncoder, times(1)).matches(anyString(), anyString());
         }
     }
 }
