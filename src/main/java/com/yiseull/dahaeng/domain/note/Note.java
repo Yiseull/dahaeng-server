4a712c7e0b866b86448d0a52f524bccf4b26b72e
Yiseull <omjl5123@catholic.ac.kr>
2023-04-21 01:59:54 +0900

    :sparkles: 노트 Entity 추가

---
+++ src/main/java/com/yiseull/dahaeng/domain/note/Note.java
@@ -0,0 +1,24 @@
+package com.yiseull.dahaeng.domain.note;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int noteId;
+    private String title;
+    private String startDate;
+    private String endDate;
+    private String description;
+    private int noteColor;
+
+}
